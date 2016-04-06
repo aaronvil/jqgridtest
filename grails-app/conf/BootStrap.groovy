c2bf508c9df74795e786a4658117f750fecc3f52
aaron <aaron@hawaii.edu>
2016-04-06 09:50:15 -1000

    added intial person to Roster

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,6 +3,9 @@ import roster.Roster
 class BootStrap {
 
     def init = { servletContext ->
+        if(Roster.count() == 0){
+            new Roster(name: 'Henry', position: 'assistant', department: 'sales', location: 'LA branch').save()
+        }
     }
     def destroy = {
     }
