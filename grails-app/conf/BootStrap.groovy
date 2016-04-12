a08e645c99611b8cd542f876b93077411e7411c2
aaron <aaron@hawaii.edu>
2016-04-12 09:01:01 -1000

    added more users in bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,6 +5,7 @@ class BootStrap {
     def init = { servletContext ->
         if(Roster.count() == 0){
             new Roster(name: 'Henry', position: 'assistant', department: 'sales', location: 'LA branch').save()
+            new Roster(name: 'Jake', position: 'Tech', department: 'IT', location: 'NY branch').save()
         }
     }
     def destroy = {
