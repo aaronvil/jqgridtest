a2875a04ce8b560c855aae23e7c7390f202996bf
aaron <aaron@hawaii.edu>
2016-04-14 15:21:01 -1000

    added more default user for test case.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -6,6 +6,9 @@ class BootStrap {
         if(Roster.count() == 0){
             new Roster(name: 'Henry', position: 'assistant', department: 'sales', location: 'LA branch').save()
             new Roster(name: 'Jake', position: 'Tech', department: 'IT', location: 'NY branch').save()
+            new Roster(name: 'Aaron', position: 'Intern', department: 'IT', location: 'London Branch').save()
+            new Roster(name: 'Mary', position: 'Supervisor', department: 'HR', location: 'Main Branch').save()
+            new Roster(name: 'Alice', position: 'CFO', department: 'Marketing', location: 'London Branch').save()
         }
     }
     def destroy = {
