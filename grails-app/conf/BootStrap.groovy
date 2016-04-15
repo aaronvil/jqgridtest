c65501f6542d59e61edf1b112557cbc1655f123c
aaron <aaron@hawaii.edu>
2016-04-14 16:32:42 -1000

    changed to include new location. Added LA and updated bootstraps for roster.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,14 +1,19 @@
+import roster.Location
 import roster.Roster
 
 class BootStrap {
-
+    def LA
     def init = { servletContext ->
         if(Roster.count() == 0){
-            new Roster(name: 'Henry', position: 'assistant', department: 'sales', location: 'LA branch').save()
-            new Roster(name: 'Jake', position: 'Tech', department: 'IT', location: 'NY branch').save()
-            new Roster(name: 'Aaron', position: 'Intern', department: 'IT', location: 'London Branch').save()
-            new Roster(name: 'Mary', position: 'Supervisor', department: 'HR', location: 'Main Branch').save()
-            new Roster(name: 'Alice', position: 'CFO', department: 'Marketing', location: 'London Branch').save()
+            new Roster(name: 'Henry', position: 'assistant', department: 'sales', location: LA).save()
+            new Roster(name: 'Jake', position: 'Tech', department: 'IT', location: LA).save()
+            new Roster(name: 'Aaron', position: 'Intern', department: 'IT', location: LA).save()
+            new Roster(name: 'Mary', position: 'Supervisor', department: 'HR', location: LA).save()
+            new Roster(name: 'Alice', position: 'CFO', department: 'Marketing', location: LA).save()
+        }
+
+        if(Location.count() == 0){
+             LA =  new Location(city: "Los Angles", state: "California", country: "USA", zipcode: "90001").save()
         }
     }
     def destroy = {
